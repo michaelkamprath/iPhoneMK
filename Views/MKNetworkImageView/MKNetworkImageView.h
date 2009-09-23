40ceb0d7f20e6282aaa1d5672eaa88827a5c26a1
Michael Kamprath <michael@meimei-2.local>
2009-09-23 00:21:55 -0700

    Added MKNetworkImageView

---
+++ Views/MKNetworkImageView/MKNetworkImageView.h
@@ -0,0 +1,46 @@
+//
+// MKNetworkImageView.h
+//
+// Copyright 2009 Michael F. Kamprath
+// michael@claireware.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+// MKNetworkImageView
+// ==================
+//
+// A very simple UIImageView-like class that allows you to set an image based on a URL. While we are waiting 
+// for the image to be loaded from the URL, a acitivity indicator will be displayed against the background color 
+// of the view.
+//
+// The basic concept of use for this class is to set the imageURL to an valid URL, the view will then display a activity indicator
+// the image's data is loaded and the image is displayed. To change the image, simply set the imageURL to something else. You
+// can also set the image directly through the image property. As a result of this, one should not treat the imageURL property as 
+// always being the correct URL for the current image being displayed.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MKNetworkImageView : UIView 
+{
+	NSURL* _imageURL;
+	UIImageView* _imageView;
+	UIActivityIndicatorView* _activityIndicator;
+	UIColor* _fillColor;
+}
+@property (retain) NSURL* imageURL;
+@property (retain) UIImage* image;
+
+@end
---
+++ Views/MKNetworkImageView/MKNetworkImageView.m
@@ -0,0 +1,173 @@
+//
+// MKNetworkImageView.m
+//
+// Copyright 2009 Michael F. Kamprath
+// michael@claireware.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "MKNetworkImageView.h"
+
+
+
+@interface MKNetworkImageView ()
+
+- (void)initObject;
+
+@end
+
+@implementation MKNetworkImageView
+@synthesize imageURL=_imageURL;
+@dynamic image;
+
+- (id)initWithFrame:(CGRect)inFrame 
+{
+    if (self = [super initWithFrame:inFrame]) 
+	{
+		[self initObject];
+	}
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+	if ( self = [super initWithCoder:decoder] )
+	{
+		[self initObject];
+	}
+	
+	return self;
+}
+
+- (void)initObject
+{
+	// set the view's actual background color clear.
+	[super setBackgroundColor:[UIColor clearColor]];
+	
+	CGRect viewFrame = CGRectMake( 0, 0, self.frame.size.width, self.frame.size.height );
+	
+	_imageView = [[UIImageView alloc] initWithFrame:viewFrame];
+	[self addSubview:_imageView];
+	
+	_activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+	_activityIndicator.center = CGPointMake( viewFrame.size.width/2.0, viewFrame.size.height/2.0);
+	[self addSubview:_activityIndicator];
+	
+}
+
+
+- (void)dealloc 
+{
+	[_fillColor release];
+	[_imageURL release];
+	[_activityIndicator release];
+	
+    [super dealloc];
+}
+
+- (void)drawRect:(CGRect)rect 
+{
+	CGContextRef curContext = UIGraphicsGetCurrentContext();
+	
+	UIColor* fillColor;
+	
+	if ( nil == self.image )
+	{
+		fillColor = _fillColor;
+	}
+	else
+	{
+		fillColor = [UIColor clearColor];
+	}
+	
+		
+	CGContextSetFillColorWithColor( curContext, fillColor.CGColor );
+	
+	CGContextFillRect( curContext, self.bounds );
+}
+
+
+
+
+- (void)setImageURL:(NSURL*)inURL
+{
+	[_imageURL release];
+	
+	if ( inURL != nil )
+	{
+		_imageURL = [inURL retain];
+		[_activityIndicator startAnimating];
+		[NSThread detachNewThreadSelector:@selector(downloadAndSetImageFromURL:) toTarget:self withObject:_imageURL];
+	}
+	else
+	{
+		_imageURL = nil;
+		self.image = nil;
+		
+		[self setNeedsDisplay];
+	}
+}
+
+- (void)setImage:(UIImage*)inImage
+{
+	if ( inImage != nil )
+	{
+		[_activityIndicator stopAnimating];
+	}
+	
+	[self setNeedsDisplay];
+	
+	_imageView.image = inImage;
+}
+
+- (UIImage*)image
+{
+	return _imageView.image;
+}
+
+- (void)downloadAndSetImageFromURL:(NSURL *)inURL
+{
+	// You must create a autorelease pool for all secondary threads.
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	
+	NSData* imageData = [[NSData alloc] initWithContentsOfURL:inURL];
+	
+	if (imageData != nil)
+	{
+		UIImage* image = [[UIImage alloc] initWithData:imageData];
+		
+		if ( image != nil )
+		{
+			[self performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:YES];
+		
+			[image release];
+		}
+		
+		[imageData release];
+	}
+	
+	[pool release];
+
+}
+
+//
+// replace the super::setBackgroundColor so that it updates the _fillColor instead.
+// 
+- (void)setBackgroundColor:(UIColor *)inColor
+{
+	[_fillColor release];
+	_fillColor = [inColor retain];
+}
+
+@end
