c3b6784e6b195396f9991ffd1e9bf85ebb2ae407
Michael Kamprath <michael@claireware.com>
2012-07-09 07:28:49 -0700

    Started a working branch for MKSocialShareTableViewCell

---
+++ TableViewCells/MKSocialShareTableViewCell/MKSocialShareTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MKSocialShareTableViewCell.h
+//  iPhoneMK Sample App
+//
+//  Created by Michael Kamprath on 7/8/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKSocialShareTableViewCell : UITableViewCell
+
+@end
---
+++ TableViewCells/MKSocialShareTableViewCell/MKSocialShareTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  MKSocialShareTableViewCell.m
+//  iPhoneMK Sample App
+//
+//  Created by Michael Kamprath on 7/8/12.
+//
+//
+
+#import "MKSocialShareTableViewCell.h"
+
+@implementation MKSocialShareTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
