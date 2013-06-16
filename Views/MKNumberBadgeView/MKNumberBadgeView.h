c46ca2b9e65937338248667140b74f8871b9afb3
Michael Kamprath <michael@claireware.com>
2013-06-16 10:58:03 -0700

    First cut at getting sample app to run under iOS 7 & Xcode 5

--- Views/MKNumberBadgeView/MKNumberBadgeView.h
+++ Views/MKNumberBadgeView/MKNumberBadgeView.h
@@ -69,7 +69,7 @@
 @property (retain,nonatomic) UIColor* textColor;
 
 // How the badge image hould be aligned horizontally in the view. 
-@property (assign,nonatomic) UITextAlignment alignment;
+@property (assign,nonatomic) NSTextAlignment alignment;
 
 // Returns the visual size of the badge for the current value. Not the same hing as the size of the view's bounds.
 // The badge view bounds should be wider than space needed to draw the badge.
